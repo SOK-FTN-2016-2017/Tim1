ad215ae6bf7d25954b1d405b2fef93aa88ba3980
imtnez <imartinezcolmenero@gmail.com>
2017-06-05 20:08:17 +0200

    Merge branch 'master' of https://github.com/SOK-FTN-2016-2017/Tim1
    # Conflicts:
    #	InitialProject/modul.py

--- InitialProject/modul.py
+++ InitialProject/modul.py
@@ -1,13 +1,5 @@
 if __name__ == '__main__':
     print("Empty module")
 
-    #First change
-    #second change
-    #third commit tryj
-    #push
-    #push second
-    ilfjglj
-    ug
-    9ug
+
     print("hola mozcdos")
-    next try
