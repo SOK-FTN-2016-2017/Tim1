4d515345cbfd0ac246646b8b5726ef58d4c66abf
imtnez <imartinezcolmenero@gmail.com>
2017-07-03 19:26:48 +0200

    Returning html page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Working poll")
+    #return HttpResponse("Working poll")
+    return render_to_response('polls/test/index.html')
