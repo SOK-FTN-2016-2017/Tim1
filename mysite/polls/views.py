b53ae12b7062c99c27342b03e2e43eb53310b940
imtnez <imartinezcolmenero@gmail.com>
2017-07-03 19:44:18 +0200

    Bux fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,7 +14,7 @@ def index(request):
 from django.http import HttpResponse
 import datetime
 
-def current_datetime(request):
+def index(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
