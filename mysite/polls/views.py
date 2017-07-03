7a911981b25b360536f0c476bd6f55d610f73786
imtnez <imartinezcolmenero@gmail.com>
2017-07-03 19:38:55 +0200

    Trying to set a plain HTML as response

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-
+#from django.template import Context, loader
+#from django.shortcuts import render_to_response
 
 def index(request):
-    #return HttpResponse("Working poll")
-    return render_to_response('polls/test/index.html')
+    return HttpResponse("Working poll")
+    #return render_to_response('test/index.html')
+    #template = loader.get_template("test/index.html")
+    #return HttpResponse(template.render())
