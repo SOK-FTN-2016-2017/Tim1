91e667b6a9107d079dd1f0d95d670e429e898f09
indalecio <jfgarciagomez@gmail.com>
2017-08-09 13:18:07 +0200

    test1dk

--- mysite/polls/jf test/ksonmaker.py
+++ mysite/polls/jf test/ksonmaker.py
@@ -8,9 +8,11 @@ s = '\n'
 for a in input.readline():
     print ("a=",a[0])
     if a[0]=='1':
-       s = s+ '{\n "name": "'+ input.readline()+'",\n"children": [\n    '
+       s = s+ '{\n "name": "'+ input.readline()
+       s = s[:len(s)-1] +'",\n"children": [\n    '
     if a[0]=='2':
-       s=s+'\n     {"name": "'+input.readline()+'"'
+       s=s+'\n     {"name": "'+input.readline()
+       s = s[:len(s)-1] + '"'
     if a[0]=='3':
        s=s+'},'
     if a[0]=='4':
