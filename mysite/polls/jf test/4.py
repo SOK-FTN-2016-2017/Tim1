85a95768ad89de2c0c34b121c8ca230bb59869e6
jfgarciagomez <jfgarciagomez@gmail.com>
2017-09-09 15:23:56 +0200

    create and modification of 4.py(middleFile creator)

--- mysite/polls/jf test/4.py
+++ mysite/polls/jf test/4.py
@@ -3,53 +3,33 @@ from html.parser import *
 
 
 class MyHTMLParser(HTMLParser):
-    def handle_data(self, dat):
-        global cont
-        global data
-        print('1')
-        if (dat[0] != '\n'):# and (dat[0:1] != '  '):
-            print('DATA=',dat)
-            cont=cont+dat
-            data=data[len(data)-1]+'= '+dat[0:15]
-        else:
-            cont=''
-        print('CONT=', cont)
-
-    #######################################################################################
     def handle_starttag(self, tag, attrs):
-        print ("2")
+
         global o
         global data
-        global cont
-#        if tag != 'meta'
-#            o=o+'O'
-#            data = data + tag + '\n'
-
-
+        if (tag != 'meta') and (tag !='link'):
+            o=o+'O'
+            data = data + tag + '\n'
+#           print ('number of attrs:',len(attrs))
+#             if (len(attrs) > 0) and (tag !='script'):
+#                 i = 0
+#                 print('antesdelwhile nu hiperatt', len(attrs))
+#                 while i < len(attrs):
+#                     o = o + 'O' + 'D'
+#                     print ('i=', i)
+#                     print ('number of attrs:', len(attrs[i]))
+#                     if len(attrs[i]) > 1:
+#                         data = data + attrs[i][0] + ': ' + attrs[i][1] + '\n'
+#                     else:
+#                         data = data + attrs[i][0] + '\n'
+#                     print ('X')
+#                     # print ('tam attrs:', len(attrs[2]))
+#                     print ("Encountered a attrvs:", attrs)  # tag)
+#                     i += 1
         # o=o+'O'
-        # if cont != '':
-        #     print ('2cont=', cont)
-        #     data = data + tag+': '+cont+'\n'
-        # else:
-        #     data = data + tag+'\n'
-        data = data + tag + '\n'
-
-        print('datra=',data)
+        # data = data + tag+'\n'
 
 
-
-#        print ('number of attrs:',len(attrs))
-#         if len(attrs) > 0:
-#             i=0
-#             while i < len(attrs):
-#                 o=o+'O'+'D'
-# #                print (i)
-#  #               print ('number of attrs:', len(attrs))
-#                 data=data + attrs[i][0]+': '+attrs[i][1]+'\n'
-# #                print ('X')
-# #                print ('tam attrs:', len(attrs[2]))
-# #                print ("Encountered a attrvs:", attrs)  # tag)
-#                 i += 1
     #######################################################################################
 
     def handle_endtag(self, tag):
@@ -58,16 +38,16 @@ class MyHTMLParser(HTMLParser):
         global o
         global data
 
-#        if tag != 'meta'
-#            o=o+'O'
+        if (tag != 'meta') and (tag !='link'):
+            o=o+'D'
 
-        o=o+'D'
+        # o=o+'D'
 
 
 data = '\n'
 o = ''
 p = ''
-cont = ''
+
 
 f=open(sys.argv[1],"r")
 s=f.read()
@@ -75,7 +55,7 @@ parser = MyHTMLParser()
 parser.feed(s)
 
 n=0
-#print(o)
+print(o)
 while n<(len(o)-1):
     if ((o[n]=='O')and(o[n+1]=='O')):
         p=p+'1'
@@ -89,9 +69,9 @@ while n<(len(o)-1):
 
 p=p + data
 
-print(data)
-# print(cont)
+print(p)
 
 outfile = open('MiddleFile', 'w')
 outfile.write(p)
 outfile.close()
+
